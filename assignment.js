347262785594c47e57758dc20a937a9142ce0c76
Anirudhya <anirudhyaduyti@gmail.com>
2022-02-05 04:09:15 +0600

    2done

--- assignment.js
+++ assignment.js
@@ -1,9 +1,10 @@
 // problem - 1: anaToVori
 function anaToVori(ana) {
+    // check if parameter is a positive number
     if (typeof ana == 'number' && ana > 0) {
-        return ana / 16;
+        return ana / 16;        // 1 vori = 16 ana
     }
-    return "Invalid Parameter! Please Pass A Positive Number";
+    return "Invalid Parameter! Please Pass A Positive Number as Aana";
 }
 
 // problem - 2: pandaCost
@@ -11,6 +12,12 @@ function pandaCost(noOfShingara, noOfShomucha, noOfJilapi) {
     const shingaraPrice = 7;
     const shomuchaPrice = 10;
     const jilapiPrice = 15;
+
+    if (typeof noOfShingara != 'number' || typeof noOfShomucha != 'number' || typeof noOfJilapi != 'number'
+        || noOfShingara < 0 || noOfShomucha < 0 || noOfJilapi < 0
+        || !Number.isInteger(noOfShingara) || !Number.isInteger(noOfShomucha) || !Number.isInteger(noOfJilapi)) {
+        return "Invalid Parameter! Please Pass Positive Integer Numbers";
+    }
     return (shingaraPrice * noOfShingara) + (shomuchaPrice * noOfShomucha) + (jilapiPrice * noOfJilapi);
 }
 
@@ -42,4 +49,4 @@ function oddFriend(names) {
     return "No Odd Friend Found";
 }
 
-// console.log("hello".length);
+console.log(pandaCost(5, 0, 1));

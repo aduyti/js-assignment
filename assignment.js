2f820ad8c40321cc8de356d142f350e1cdfb65cb
Anirudhya <anirudhyaduyti@gmail.com>
2022-02-05 00:51:44 +0600

    layout

--- assignment.js
+++ assignment.js
@@ -1,1 +1,23 @@
-console.log("hello");
+// console.log("hello");
+
+// problem - 1: ana to vori
+function anaToVori(ana) {
+    let vori = 0;
+    return vori;
+}
+
+// problem - 2: pandaCost
+function pandaCost(shingara, shomucha, jilapi) {
+    let totalCost = 0;
+    return totalCost;
+}
+
+// problem - 3: picnicBudget
+function picnicBudget(numberOfPeople) {
+    return totalCost;
+}
+
+// problem - 4: oddFriend
+function oddFriend(numberOfPeople) {
+    return friends;
+}
